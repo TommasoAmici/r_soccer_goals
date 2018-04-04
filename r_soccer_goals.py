91f7ca434589da31ac741a6e8e5545b3c1949d17
Tommaso Amici <tommaso.amici@gmail.com>
2018-04-04 18:19:24 +0200

    check for url, instead of poster

--- r_soccer_goals.py
+++ r_soccer_goals.py
@@ -2,34 +2,31 @@ from telegram.ext import Updater
 import telegram
 import praw
 import sched, time
-
-
-teams = ['milan', 'inter', 'napoli', 'juventus', 'roma',
-         'lazio', 'spal', 'udinese', 'sampdoria',  'sassuolo',
-         'benevento', 'cagliari', 'crotone', 'verona',
-         'hellas', 'atalanta', 'chievo', 'torino', 'fiorentina',
-         'bologna']
+import logging
 
 
 def is_goal(post):
-    if post.author == 'AutoModerator' or post.author == 'MatchThreadder':
+    teams = ['milan', 'inter', 'napoli', 'juve', 'roma',
+             'lazio', 'spal', 'udinese', 'sampdoria', 'sassuolo',
+             'benevento', 'cagliari', 'crotone', 'verona',
+             'hellas', 'atalanta', 'chievo', 'torino', 'fiorentina',
+             'bologna']
+    to_drop = ['internacional', 'match']
+    if any(drop in post.title.lower() for drop in to_drop):
         return False
-    elif post.author == 'Meladroit1' or post.author == 'gemifra' or post.author == 'biffmila':
-        if 'internacional' in post.title.lower():
-            return False
-        else:
-            for team in teams:
-                if team in post.title.lower():
-                    return True
     else:
-        return False
+        for team in teams:
+            if team in post.title.lower():
+                return True
+            else:
+                continue
 
 
 def aa_mirror(post):
     mirrors = []
-    for comment in post.comments:
-        if 'mirror' in comment.body.lower() or 'replay' in comment.body.lower() and len(comment.body) > 30:
-            mirrors.append(comment)
+    for c in post.comments:
+        if ('mirror' in c.body.lower() or 'replay' in c.body.lower() and len(c.body) > 30) and 'aa' in c.body.lower():
+            mirrors.append(c)
         else:
             continue
     return mirrors
@@ -37,20 +34,19 @@ def aa_mirror(post):
 
 def check_soccer_new(reddit):
     to_channel = []
-    for post in reddit.subreddit('soccer').new(limit=5):
-        if len(post.title) > 100:
-            continue
+    streams = ['streamable', 'flixtc', 'mixtape', 'v.reddit', 'clippituser', 'streamja']
+    posts = [p for p in reddit.subreddit('soccer').new(limit=15) if any(s in p.url for s in streams)]
+    for post in posts:
+        if is_goal(post):
+            to_channel.append(post)
+            try:
+                to_channel.append(aa_mirror(post))
+            except:
+                pass
+            post.hide()
+            break
         else:
-            if is_goal(post):
-                to_channel.append(post)
-                try:
-                    to_channel.append(aa_mirror(post))
-                except:
-                    pass
-                post.hide()
-                break
-            else:
-                continue
+            continue
     return to_channel
 
 
@@ -69,13 +65,20 @@ def check_goals_sched():
             try:
                 bot.send_document(chat_id='@golledisoccer', document=post.url, caption=post.title, disable_notification=True)
             except:
-                bot.send_message(chat_id='@golledisoccer', text="{}\n{}".format(post.title,post.url), disable_notification=True)            
+                bot.send_message(chat_id='@golledisoccer',
+                                 text="{}\n{}".format(post.title, post.url),
+                                 parse_mode='markdown', disable_notification=True)            
         elif isinstance(post, list):
             for comment in post:
-                bot.send_message(chat_id='@golledisoccer', text=comment.body, parse_mode="markdown", disable_notification=True)
+                bot.send_message(chat_id='@golledisoccer',
+                                 text='**AA/Mirror**\n{}'.format(comment.body),
+                                 parse_mode="markdown",
+                                 disable_notification=True)
     s.enter(5, 1, check_goals_sched)
 
 
-s = sched.scheduler(time.time, time.sleep)    
-s.enter(5, 1, check_goals_sched)
-s.run()
+if __name__ == "__main__":
+    logging.info('Starting /r/soccer_golle_bot')
+    s = sched.scheduler(time.time, time.sleep)    
+    s.enter(5, 1, check_goals_sched)
+    s.run()
