52224b58a21dd08536fdfeaefe84c7bf656b25d4
Tommaso A <tommaso.amici@gmail.com>
2018-07-05 15:18:04 +0200

    forgot quotes in dictionary

--- settings.py
+++ settings.py
@@ -4,14 +4,14 @@ Stores all secret keys for services and application
 
 
 reddit_settings = {
-    client_id: "",
-    client_secret: "",
-    user_agent: "",
-    username: "",
-    password: "",
-    subreddit: "",
+    "client_id": "",
+    "client_secret": "",
+    "user_agent": "",
+    "username": "",
+    "password": "",
+    "subreddit": "",
 }
 
-telegram_settings = {chat_id: "", bot_token: ""}
+telegram_settings = {"chat_id": "", "bot_token": ""}
 
-streamable_settings = {email: "", password: ""}
+streamable_settings = {"email": "", "password": ""}
