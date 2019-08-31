f18e6ebb94a34d684d34a7e2ffe0c2fdbde0fe39
Tommaso A <tommaso.amici@me.com>
2019-08-31 12:15:11 +0200

    added new source to list

--- r_soccer_goals.py
+++ r_soccer_goals.py
@@ -53,6 +53,7 @@ def is_video(post):
         "kyouko.se",
         "streamvi",
         "twitter",
+        "sporttube",
     )
     if any(s in post.url for s in streams):
         if is_goal(post):
