ca20fcee1b9be82ae0d9e1c515a4cd88acf7334d
Tommaso Amici <tommaso.amici@me.com>
2020-07-23 17:52:59 +0200

    Reddit praw instance can be readonly

--- r_soccer_goals.py
+++ r_soccer_goals.py
@@ -87,8 +87,6 @@ def main():
             client_id=os.environ["REDDIT_CLIENT_ID"],
             client_secret=os.environ["REDDIT_CLIENT_SECRET"],
             user_agent=os.environ["REDDIT_USER_AGENT"],
-            username=os.environ["REDDIT_USERNAME"],
-            password=os.environ["REDDIT_PASSWORD"],
         )
     except:
         main()
