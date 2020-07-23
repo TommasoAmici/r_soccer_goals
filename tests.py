03fbdac066d147b47cb92fcb5f6cc7277f255b04
Tommaso Amici <tommaso.amici@me.com>
2020-07-23 17:26:54 +0200

    Added tests

---
+++ tests.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+import pytest
+
+from r_soccer_goals import is_goal, is_video
+
+
+@dataclass
+class Post:
+    title: str
+    url: str
+
+    def __init__(self, title: str, url: str):
+        self.title = title
+        self.url = url
+
+
+def test_is_goal():
+    p = Post("Juventus 1-[2] Torino Belotti great goal", "streamja/best-goal")
+    assert is_goal(p) is True
+    p = Post("Juventus Primavera [7]-2 Albinoleffe Dybala", "gazzetta.it")
+    assert is_goal(p) is False
+
+
+def test_is_video():
+    p = Post("Juventus 1-[2] Torino Belotti great goal", "streamja/best-goal")
+    assert is_video(p) is True
+    p = Post("Juventus Primavera [7]-2 Albinoleffe Dybala", "gazzetta.it")
+    assert is_video(p) is False
