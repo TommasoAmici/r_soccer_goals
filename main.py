ab4c0b9c95e5dd1053e6d8a43a6f4f4be9dbbdb8
Tommaso Amici <me@tommasoamici.com>
2022-08-18 22:31:23 +0200

    fix: wait 30 seconds before attempting to get URL with yt-dlp
    Sometimes videos on Reddit haven't been processed yet and yt-dlp
    fails to extract the URL. Adding a wait time of 30 seconds should
    prevent this class of errors

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ logger.setLevel(logging.INFO)
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
-def get_url(submission: Submission) -> Optional[str]:
+def get_url(video_url: str) -> Optional[str]:
     result = {}
     with yt_dlp.YoutubeDL({"quiet": True, "no_check_certificate": True}) as ydl:
         # mostly to handle tweets
         try:
-            result = ydl.extract_info(submission.url, download=False)
-        except Exception as e:
-            logger.error(e)
+            result = ydl.extract_info(video_url, download=False)
+        except:
+            logger.error("Failed to extract video URL")
             return None
     if "entries" in result:
         # Can be a playlist or a list of videos
@@ -60,64 +60,70 @@ def is_video(submission: Submission) -> bool:
     return False
 
 
-async def send_video(bot: Bot, submission: Submission, url: str) -> None:
-    logger.info("sending video", submission.title, url)
-    bot.send_video(
-        chat_id=os.environ["TELEGRAM_CHAT_ID"],
-        video=url,
-        caption=submission.title,
-        disable_notification=True,
-    )
-
-
-async def process_submission(bot: Bot, submission: Submission) -> None:
+async def send(bot: Bot, submission: Submission):
     """
-    For each submission
-    - determines if it's a goal
-    - extracts direct url
-    - sends to telegram channel
+    For each item in queue, extracts direct url and sends it to telegram channel
     """
-    if is_video(submission):
+    try:
+        url = get_url(submission.url)
+    except KeyError:
+        return
+    if url is not None:
+        logger.debug(f"sending video {url}")
         try:
-            url = get_url(submission)
-        except KeyError:
-            return
+            await bot.send_video(
+                chat_id=os.environ["TELEGRAM_CHAT_ID"],
+                video=url,
+                caption=submission.title,
+                disable_notification=True,
+            )
+        except:
+            logger.exception("Failed to send video URL to channel")
+            # if it fails to send video, send a link
+            # don't send tweets as links
+            if "twitter" not in submission.url:
+                logger.info("sending as message", submission.title, submission.url)
+                await bot.send_message(
+                    chat_id=os.environ["TELEGRAM_CHAT_ID"],
+                    text=f"{submission.title}\n\n{submission.url}",
+                    disable_notification=True,
+                )
 
-        if url is not None:
-            try:
-                await send_video(bot, submission, url)
-            except Exception as e:
-                logger.error(e)
-                # if it fails to send video, send a link
-                # don't send tweets as links
-                if "twitter" not in submission.url:
-                    logger.info("sending as message", submission.title, submission.url)
-                    bot.send_message(
-                        chat_id=os.environ["TELEGRAM_CHAT_ID"],
-                        text=f"{submission.title}\n\n{submission.url}",
-                        disable_notification=True,
-                    )
+
+async def worker(queue: asyncio.Queue, bot: Bot):
+    while True:
+        submission: Submission = await queue.get()
+        logger.debug(f"processing from queue: {submission.id}")
+        await asyncio.sleep(30)
+        await send(bot, submission)
+        queue.task_done()
 
 
 async def main() -> None:
-    logger.info("Started r_soccer_goals bot")
-    try:
-        bot = Bot(token=os.environ["TELEGRAM_BOT_TOKEN"])
-        reddit = asyncpraw.Reddit(
-            client_id=os.environ["REDDIT_CLIENT_ID"],
-            client_secret=os.environ["REDDIT_CLIENT_SECRET"],
-            user_agent=os.environ["REDDIT_USER_AGENT"],
-            password=os.environ["REDDIT_PASSWORD"],
-            username=os.environ["REDDIT_USERNAME"],
-        )
-        reddit.read_only = True
-        subreddit = await reddit.subreddit(os.environ["REDDIT_SUBREDDIT"])
-        async for submission in subreddit.stream.submissions(skip_existing=True):
-            logger.info(f"Processing post {submission.id}")
-            await process_submission(bot, submission)
-    except Exception as e:
-        logger.error(e)
+    bot = Bot(token=os.environ["TELEGRAM_BOT_TOKEN"])
+
+    queue = asyncio.Queue()
+    # Create three worker tasks to process the queue concurrently.
+    tasks = []
+    for i in range(3):
+        task = asyncio.create_task(worker(queue, bot))
+        tasks.append(task)
+
+    reddit = asyncpraw.Reddit(
+        client_id=os.environ["REDDIT_CLIENT_ID"],
+        client_secret=os.environ["REDDIT_CLIENT_SECRET"],
+        user_agent=os.environ["REDDIT_USER_AGENT"],
+        password=os.environ["REDDIT_PASSWORD"],
+        username=os.environ["REDDIT_USERNAME"],
+    )
+    reddit.read_only = True
+    subreddit = await reddit.subreddit(os.environ["REDDIT_SUBREDDIT"])
+    async for submission in subreddit.stream.submissions(skip_existing=True):
+        logger.debug(f"Processing post {submission.id}")
+        if is_video(submission):
+            await queue.put(submission)
 
 
 if __name__ == "__main__":
+    logger.info("Started r_soccer_goals bot")
     asyncio.run(main())
